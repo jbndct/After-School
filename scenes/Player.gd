1efc35ae68c0f319f154a765614f4c790f61e934
John Benedict Baladia <johnbenedict.baladia@gmail.com>
2026-03-15 21:26:55 +0800

    Added a moving rectangle in a floor

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,37 +2,31 @@ extends CharacterBody2D
 
 const SPEED = 180.0
 const GRAVITY = 800.0
-var facing_right: bool = true
-var scene_end_x: float = 3200.0
 
+var scene_end_x: float = 3200.0
 signal reached_end
 
+var debug_label: Label
+
+func _ready() -> void:
+	up_direction = Vector2.UP
+	
 func _physics_process(delta: float) -> void:
-	# apply gravity
+	# 1. Gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-	else:
-		velocity.y = 0
-	
-	var direction = get_input_direction()
+
+	# 2. Check standard arrow keys
+	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction != 0:
 		velocity.x = direction * SPEED
-		facing_right = direction > 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+		
+	# 3. Apply physics
 	move_and_slide()
-	
-	global_position.x = max(80.0, global_position.x)
-	
+
+	# 4. End of scene check
 	if global_position.x >= scene_end_x:
 		emit_signal("reached_end")
-
-func get_input_direction() -> float:
-	if Input.is_action_pressed("ui_right"):
-		return 1.0
-	return 0.0
-	
-func _ready() -> void:
-	up_direction = Vector2.UP
