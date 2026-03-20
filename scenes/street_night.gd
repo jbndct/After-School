02cb407ca4230e5271501ac300ae0814da37f794
John Benedict Baladia <johnbenedict.baladia@gmail.com>
2026-03-20 11:10:57 +0800

    Modified camera of player in street morning day 1 scene

---
+++ scenes/street_night.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@export var next_scene: String = "res://scenes/room_day1.tscn"
+
+func _ready() -> void:
+	# parallax layers
+	$ParallaxBackground/"ParallaxLayer (far)"/ColorRect.size = Vector2(3840, 720)
+	$ParallaxBackground/"ParallaxLayer (mid)"/ColorRect.size = Vector2(3840, 720)
+	$ParallaxBackground/"ParallaxLayer (near)"/ColorRect.size = Vector2(3840, 720)
+	
+	# player rectangle and starting position
+	$Player/ColorRect.size = Vector2(32, 52)
+	$Player/ColorRect.position = Vector2(16, -52)
+	$Player.position = Vector2(800, 600)
+	
+	# floor rectangle
+	$Floor/ColorRect.size = Vector2(4000, 20)
+	$Floor/ColorRect.position = Vector2(-2000, -10)
+	$Floor.position = Vector2(0, 640)
+	
+	# force the floor collision shape size
+	var floor_shape = RectangleShape2D.new()
+	floor_shape.size = Vector2(4000, 20)
+	$Floor/CollisionShape2D.shape = floor_shape
+	
+	# force the player collision shape size
+	var player_shape = RectangleShape2D.new()
+	player_shape.size = Vector2(32, 52)
+	$Player/CollisionShape2D.shape = player_shape
+	
+	# Force the invisible collision boxes to perfectly match the drawn rectangles
+	$Floor/CollisionShape2D.position = Vector2(0, 0)
+	$Player/CollisionShape2D.position = Vector2(0, -26)
+		
+	$Player.reached_end.connect(_on_player_reached_end)
+
+func _on_player_reached_end() -> void:
+	get_tree().change_scene_to_file(next_scene)
