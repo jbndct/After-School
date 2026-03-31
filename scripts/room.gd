7898efb8d05a1faae877a0b74a07a68316fb5662
John Benedict Baladia <johnbenedict.baladia@gmail.com>
2026-03-31 20:02:33 +0800

    room finished

--- scripts/room.gd
+++ scripts/room.gd
@@ -1,18 +1,41 @@
 extends Node2D
 
 @onready var dialogue_box = $DialogueBox
+@onready var objective_label = $ObjectiveLabel
+@onready var interact_prompt = $InteractPrompt
+@onready var door_prompt = $DoorPrompt
 
 var player_in_interact_zone: bool = false
+var player_at_door: bool = false
 var has_talked: bool = false
 
 func _ready() -> void:
-	# We leave this empty now. The player must trigger the dialogue manually.
-	pass
+	# Hide interaction prompts by default
+	interact_prompt.visible = false
+	door_prompt.visible = false
+	
+	# Set the initial objective based on the part
+	match GameState.current_part:
+		1: objective_label.text = "Objective: Figure out what to do about tuition."
+		2: objective_label.text = "Objective: Prepare to look for a job."
+		3: objective_label.text = "Objective: Get ready for work."
+		4: objective_label.text = "Objective: Check your balance."
 
-# Listens for the interact button
-func _input(event: InputEvent) -> void:
-	if player_in_interact_zone and not has_talked and event.is_action_pressed("ui_accept"):
-		play_room_dialogue()
+# Use _unhandled_input so the dialogue box gets priority if it's open
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		if player_in_interact_zone and not has_talked:
+			# Hide the prompt once they interact
+			interact_prompt.visible = false 
+			play_room_dialogue()
+		elif player_at_door:
+			if has_talked:
+				GameState.advance_scene()
+			else:
+				# Temporarily change objective text to warn the player
+				objective_label.text = "I shouldn't leave until I clear my head."
+				await get_tree().create_timer(2.0).timeout
+				_ready() # Reset objective text
 
 func play_room_dialogue() -> void:
 	var dialogue_lines = []
@@ -30,49 +53,39 @@ func play_room_dialogue() -> void:
 				{ "speaker": "Dominador", "text": "It's a trap. I know it is." },
 				{ "speaker": "Dominador", "text": "I just need to keep my head down, finish this night shift, and not do anything stupid." }
 			]
-		2: 
-			dialogue_lines = [
-				{ "speaker": "Dominador", "text": "I need to find a job today." } 
-			]
-		3: 
-			dialogue_lines = [
-				{ "speaker": "Dominador", "text": "Time to head to work." } 
-			]
-		4: 
-			if GameState.hand < 15000:
-				dialogue_lines = [
-					{ "speaker": "Dominador", "text": "No... no, no, no. Where did it all go?" },
-					{ "speaker": "Dominador", "text": "Balance: ₱" + str(GameState.hand) + ". This can't be right. I just had it." },
-					{ "speaker": "Dominador", "text": "I was just trying to get the rest of the tuition. What did I just do?" },
-					{ "speaker": "Dominador", "text": "How am I supposed to tell Ma? I'm completely screwed." }
-				]
-			else:
-				dialogue_lines = [
-					{ "speaker": "Dominador", "text": "Shift's finally over. My head is pounding." },
-					{ "speaker": "Dominador", "text": "Total balance: ₱" + str(GameState.hand) + "." },
-					{ "speaker": "Dominador", "text": "It's enough. I actually held on to it." },
-					{ "speaker": "Dominador", "text": "I just need to drag myself to the registrar before I collapse." }
-				]
+		# (Keep your other match cases 2, 3, 4 the exact same as before)
 				
 	if dialogue_lines.size() > 0:
 		dialogue_box.play(dialogue_lines, _on_dialogue_finished)
 
 func _on_dialogue_finished() -> void:
-	has_talked = true # Unlocks the door
+	has_talked = true
+	objective_label.text = "Objective: Head out."
+	
+	# If they are already standing at the door when the dialogue finishes
+	if player_at_door:
+		door_prompt.visible = true
 
 # --- SIGNAL CALLBACKS ---
 
 func _on_interactable_item_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and not has_talked:
 		player_in_interact_zone = true
+		interact_prompt.visible = true
 
 func _on_interactable_item_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player_in_interact_zone = false
+		interact_prompt.visible = false
 
 func _on_exit_door_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		player_at_door = true
+		# Only show the exit prompt if they've finished the mandatory interaction
 		if has_talked:
-			GameState.advance_scene()
-		else:
-			print("Player needs to interact first!") # Just for testing so you can see it in the console
+			door_prompt.visible = true
+
+func _on_exit_door_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_at_door = false
+		door_prompt.visible = false
